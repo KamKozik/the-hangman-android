9eec1deaba9b8caf5591a4b84a5e84ed81837177
Artur Furmańczyk <furmanczyk.artur@gmail.com>
2017-11-25 18:20:22 +0100

    Switch activities on click

---
+++ app/src/main/java/algorytmiczne/swiry/GameActivity.java
@@ -0,0 +1,13 @@
+package algorytmiczne.swiry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
--- app/src/main/java/algorytmiczne/swiry/MainActivity.java
+++ app/src/main/java/algorytmiczne/swiry/MainActivity.java
@@ -2,8 +2,11 @@ package algorytmiczne.swiry;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -18,10 +21,12 @@ public class MainActivity extends Activity {
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
-//
-//        ActionBar actionBar = getActionBar();
-//        if (actionBar != null) {
-//            actionBar.hide();
-//        }
+    }
+
+    public void onPlayButtonClicked(View view) {
+//        Toast.makeText(getApplicationContext(), "Play button clicked", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this, GameActivity.class);
+        //intent.putextra
+        MainActivity.this.startActivity(intent);
     }
 }
