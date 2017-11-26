66cb669ef713d1b0c546fd0be335d945a9f130d4
Artur Furmańczyk <furmanczyk.artur@gmail.com>
2017-11-26 11:49:36 +0100

    Dynamically draw word

--- app/src/main/java/algorytmiczne/swiry/GameActivity.java
+++ app/src/main/java/algorytmiczne/swiry/GameActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.widget.AppCompatTextView;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -13,11 +14,18 @@ import android.widget.Space;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
+import org.apache.commons.lang3.text.WordUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static android.content.ContentValues.TAG;
 
 public class GameActivity extends Activity {
 
     static final String AVAILABLE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static final int LINE_MAX_WIDTH = 12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +43,11 @@ public class GameActivity extends Activity {
                 | View.SYSTEM_UI_FLAG_IMMERSIVE;
         decorView.setSystemUiVisibility(uiOptions);
 
-        fillLettersGridLayout();
+        createKeyboard();
+        drawWord("BETTER SAFE");
     }
 
-    public void fillLettersGridLayout() {
+    public void createKeyboard() {
         TableLayout table = findViewById(R.id.lettersTableLayout);
         Log.d(TAG, "Width :" + table.getWidth());
         int alphabetIndexer = 0;
@@ -51,7 +60,6 @@ public class GameActivity extends Activity {
             for (int cell = 0; cell < 7; cell++) {
                 if (row == 3 && (cell == 0 || cell == 6)) {
                     Space emptyView = new Space(this);
-                    emptyView.setBackgroundColor(Color.RED);
                     TableRow.LayoutParams viewParams = new TableRow.LayoutParams();
                     viewParams.gravity = Gravity.CENTER;
                     emptyView.setLayoutParams(viewParams);
@@ -76,5 +84,41 @@ public class GameActivity extends Activity {
             table.addView(tr, params);
         }
         table.setStretchAllColumns(true);
+    }
+
+    public void drawWord(String word) {
+        int rows = word.length() / LINE_MAX_WIDTH;
+        List<String> wordRows = new ArrayList<>();
+
+        TableLayout wordTableLayout = findViewById(R.id.wordTableLayout);
+        Log.d(TAG, "Width :" + wordTableLayout.getWidth());
+
+        String formattedString = WordUtils.wrap(word, LINE_MAX_WIDTH);
+        wordRows.addAll(Arrays.asList(formattedString.split("\\r?\\n")));
+
+        for (int row = 0; row < wordRows.size(); row++) {
+            TableRow tr = new TableRow(this);
+            tr.setId(200 + 1);
+            TableLayout.LayoutParams params = new TableLayout.LayoutParams(0, TableLayout.LayoutParams.WRAP_CONTENT, 1f);
+
+            for (char c : wordRows.get(row).toCharArray()) {
+                AppCompatTextView letterView = new AppCompatTextView(this);
+                if (c == ' ') {
+                    letterView.setBackgroundResource(R.drawable.letterspace);
+                } else {
+                    letterView.setBackgroundResource(R.drawable.letterline);
+                }
+                letterView.setText(String.valueOf(c));
+                Typeface typeface = ResourcesCompat.getFont(this, R.font.appfont);
+                letterView.setTypeface(typeface);
+                TableRow.LayoutParams letterParams = new TableRow.LayoutParams();
+                letterParams.gravity = Gravity.FILL;
+                letterView.setLayoutParams(letterParams);
+                tr.addView(letterView);
+            }
+            tr.setLayoutParams(params);
+            wordTableLayout.addView(tr, params);
+        }
+//        wordTableLayout.setStretchAllColumns(true);
     }
 }
